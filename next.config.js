47b56c93ab7b920bb3a6913ed10991d4fd96e48f
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-11-24 18:48:04 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/hello',
-  assetPrefix: '/hello',
+  basePath: '/',
+  assetPrefix: '/',
   trailingSlash: true,
 };
 
